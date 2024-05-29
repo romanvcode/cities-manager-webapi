403e5b7f812d13411ba7074c7ed50ed930b0fdca
romanvcode <roman.v.ua@gmail.com>
2024-05-29 17:23:31 +0300

    Add Angular project, Render mock data

---
+++ CitiesManager.WebAPI/ClientApp/CitiesAngularApp/src/app/services/cities.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { City } from "../models/city"
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CitiesService {
+  cities: City[] = [];
+  constructor() {
+    this.cities = [
+      new City("101", "New York"),
+      new City("102", "London"),
+      new City("103", "Paris")
+    ];
+  }
+
+  public getCities(): City[] {
+    return this.cities;
+  }
+}
